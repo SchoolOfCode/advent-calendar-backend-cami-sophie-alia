81d8289f57497c4f1fd58a0b581999ad840b46fb
aliafarhana88 <alia.abdulghaffar@gmail.com>
2022-12-09 11:57:02 +0000

    port and port listener added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import adventCalendarRouter from "./routes/adventCalendar.js";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
@@ -13,4 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/items", adventCalendarRouter);
 
+app.listen(PORT, function(){
+    console.log(`server is listening on port ${PORT}`)
+})
 export default app;
--- routes/adventCalendar.js
+++ routes/adventCalendar.js
@@ -1,5 +1,5 @@
-import {router} from 'express';
-const router = router(); 
+import {Router} from 'express';
+const adventCalendarRouter = Router(); 
 
 import getPresentbyID from "../models/adventCalendar.js"
 
@@ -7,12 +7,12 @@ import getPresentbyID from "../models/adventCalendar.js"
  * router function for a get request. will call a helper function
  * that get an item by ID. 
  */
-router.get("/", async function(req,res){
+ adventCalendarRouter.get("/", async function(req,res){
     const data = await getPresentbyID(req.params.id);
     res.json({success: true, payload: data})
 })
 
-export default router;
+export default adventCalendarRouter;
 
 
 
