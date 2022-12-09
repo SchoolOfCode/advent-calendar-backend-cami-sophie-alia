f8a74fbeb87707230da6261f781d381aea8f7f4e
camillabrizzi <camillabrizzi@icloud.com>
2022-12-09 12:17:43 +0000

    removed port and listener

--- app.js
+++ app.js
@@ -6,16 +6,9 @@ import adventCalendarRouter from "./routes/adventCalendar.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
-
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/items", adventCalendarRouter);
-
-app.listen(PORT, function () {
-  console.log(`server is listening on port ${PORT}`);
-});
-export default app;
